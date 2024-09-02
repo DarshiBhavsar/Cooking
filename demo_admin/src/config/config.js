fe654e4e31cd3e8a385c68856dceb11a7bc8009e
DarshiBhavsar14 <159266727+DarshiBhavsar14@users.noreply.github.com>
2024-09-02 14:59:51 +0530

    chages

--- demo_admin/src/config/config.js
+++ demo_admin/src/config/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://cooking-9.onrender.com'; // Change this to your actual base URL
-
+// const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://cooking-9.onrender.com'
 export default BASE_URL;
